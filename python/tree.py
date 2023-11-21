57c55ffa286c2d5289adcbccafb45791cfed1178
Ike <dylanraspino264@gmail.com>
2023-11-21 12:17:16 +0000

    updated tree

--- python/tree.py
+++ python/tree.py
@@ -1,14 +1,10 @@
-class Node:
-    def __init__(self, val):
-        self.next = Node | None
-        self.val = val
-
 
 class TreeNode:
-    def __init__(self, root):
-        self.root = root
-        self.left = Node
-        self.right = Node
+    def __init__(self, val):
+        self.parent = TreeNode 
+        self.left = TreeNode | None
+        self.right = TreeNode | None
+        self.val = val
     
     def BSTPreorder(self):
         def walk(TreeNode: curr, path):
@@ -21,7 +17,7 @@ class TreeNode:
             return path
 
         def pre_order_search(TreeNode: head):
-            return walk(head, List: path)
+            return walk(head, list: path)
 
     def BSTInorder(self):
         def walk(TreeNode: curr, path):
@@ -34,7 +30,7 @@ class TreeNode:
             return path
 
         def in_order_search(TreeNode: head):
-            return walk(head, List: path)
+            return walk(head, list: path)
 
     def BSTPostorder(self):
         def walk(TreeNode: curr, path):
