5bdf56129af44dcbb7527795d3e34d2e1f8cf983
Ike <dylanraspino264@gmail.com>
2023-12-03 14:45:46 +0000

    clean up heap sorting algortihm

---
+++ java/HeapSort.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+class Heap{
+    
+    int[] arr = {};
+    int arr_size = arr.length;
+    int last_index;
+}
+
+void HeapInsert(Heap heap, int value){
+    if(heap.last_index == heap.arr_size - 1){
+        heap.arr_size * 2;
+    }
+    heap.last_index = heap.last_index + 1;
+    heap.arr[heap.last_index] = value;
+    HeapifyUp(heap);
+}
+
+Object HeapRemoveMax(Heap heap){
+    if (heap.last_index == 0){
+        return null;
+    }
+    int result = heap.arr[1];
+    heap.arr[1] = heap.arr[heap.last_index];
+    heap.arr[last_index] = (Integer) null;
+    heap.last_index = heap.last_index - 1;
+    return HeapifyDown(heap, 1);
+}
+
+int parent(int current){
+    return current / 2;
+}
+
+void HeapifyUp(Heap heap){
+    int current = heap.last_index;
+    int parent = parent(current);
+
+    while(parent>=1 && heap.arr[parent] < heap.arr[current]){
+        int temp = heap.arr[parent];
+        heap.arr[parent] = heap.arr[current];
+        heap.arr[current] = temp;
+        current = parent;
+        parent = parent(current);
+    }
+}
+
+int HeapifyDown(Heap heap, int idx){
+    int lidx = leftChild(idx);
+    int ridx = rightChild(idx);
+    
+    while(idx <= heap.last_index){
+        int swap = idx;
+        if(lidx <= heap.last_index && heap.arr[swap] < heap.arr[lidx]){
+            swap = lidx;
+        }
+        if(ridx <= heap.last_index && heap.arr[swap] < heap.arr[ridx]){
+            swap = ridx;
+        }
+        if(idx != swap){
+            int temp = heap.arr[idx];
+            heap.arr[idx] = heap.arr[swap];
+            heap.arr[swap] = temp;
+            idx = swap;
+        } else {
+            break;
+        }
+    }
+}
+
+int leftChild(int idx){
+    return idx * 2;
+}
+
+int rightChild(int idx){
+    return idx * 2 + 1;
+}
