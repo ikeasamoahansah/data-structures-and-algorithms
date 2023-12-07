fa746b5efb6fc13938e338823edf7356229f2e2a
Ike <dylanraspino264@gmail.com>
2023-12-07 22:08:14 +0000

    heap in python

---
+++ python/heap_sort.py
@@ -0,0 +1,26 @@
+class Heap:
+    def __init__(self, arr) -> None:
+        self.array = arr
+        self.arr_size = len(arr)
+        self.last_index = -1
+
+    def insert(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def heapifyUp(self):
+        pass
+
+    def heapifyDown(self):
+        pass
+
+    def parents(self):
+        pass
+
+    def lChild(self):
+        pass
+
+    def rChild(self):
+        pass
