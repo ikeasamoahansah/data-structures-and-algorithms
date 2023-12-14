1abd6715d2103c2c9e5ee4fd86955b252fd0f8f7
Ike <dylanraspino264@gmail.com>
2023-12-14 00:29:01 +0000

    understood

--- java/InsertionSort.java
+++ java/InsertionSort.java
@@ -1,24 +1,25 @@
 public class InsertionSort {
     public static void main(String[] args) {
-        int [] s = {6, 8, 30, 1, 4, 9};
+        int s[] = {6, 8, 30, 1, 4, 9};
         insert_sort(s);
+
+        for(int i : s){
+            System.out.print(i + "");
+        }
     }
 
     static int[] insert_sort(int[] s){
-        int i = 1;
         int n = s.length;
 
-        while(i<n){
-            int current = s[i];
+        for(int i =1; i<n; i++){
+            int temp = s[i];
             int j = i - 1;
 
-            while(j>=0 && s[j]>current){
+            while(j>=0 && s[j]>temp){
                 s[j+1] = s[j];
-                j = j-1;
+                j--;
             }
-            s[j+1] = current;
-            i++;
-
+            s[j+1] = temp;
         }
         return s;
     }
