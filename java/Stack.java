83a2ecc86afd45cd5364f3c98d994d3aaa8dad52
Ike <dylanraspino264@gmail.com>
2024-01-11 23:26:12 +0000

    learning stack

--- java/Stack.java
+++ java/Stack.java
@@ -2,18 +2,18 @@ package java;
 
 class Node{
     int data;
+    Node prev;
 
     Node(int d){
         data = d;
+        prev = null;
     }
 }
 
 class Stack{
     Node head;
-    Node prev;
     Stack(Node h){
         head = h;
-        prev = null;
     }
 
     private int peek(){
@@ -21,16 +21,22 @@ class Stack{
     }
 
     private int push(Node item){
-        Node new_node = new Node(item);
+        Node new_node = new Node(item.data);
         if(this.head != null){
             new_node.prev = this.head;
             this.head = new_node;
             return this.head.data;
         }
         this.head = new_node;
+        return this.head.data;
     }
 
     private int pop(){
-
+        if(this.head != null){
+            Node head = new Node(this.head.data);
+            this.head = head.prev;
+            return head.data;
+        }
+        return -1;
     }
 }
